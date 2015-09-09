a808c751d1d0334a207c924e4e38559b10c09faa
Benj Sicam <bsicam@reekoh.com>
2015-09-09 21:06:28 +0800

    Messaging and Data Validation Enhancements
    - TCP Endpoint App-to-Device/Group of Devices Messaging
    - Minor Data Validation Enhancements

--- platform.js
+++ platform.js
@@ -3,6 +3,10 @@
 var inherits     = require('util').inherits,
 	EventEmitter = require('events').EventEmitter;
 
+var isString = function (val) {
+	return typeof val === 'string' || ((!!val && typeof val === 'object') && Object.prototype.toString.call(val) === '[object String]');
+};
+
 function Platform() {
 	if (!(this instanceof Platform)) return new Platform();
 
@@ -31,12 +35,13 @@ Platform.init = function () {
 	});
 };
 
-Platform.prototype.notifyListen = function (callback) {
+Platform.prototype.notifyReady = function (callback) {
 	setImmediate(function () {
-		callback = callback || function () {};
+		callback = callback || function () {
+			};
 
 		process.send({
-			type: 'listening'
+			type: 'ready'
 		});
 
 		callback();
@@ -45,9 +50,10 @@ Platform.prototype.notifyListen = function (callback) {
 
 Platform.prototype.notifyConnection = function (clientAddress, callback) {
 	setImmediate(function () {
-		callback = callback || function () {};
+		callback = callback || function () {
+			};
 
-		if (!clientAddress) return callback(new Error('Client IP address is required.'));
+		if (!clientAddress || !isString(clientAddress)) return callback(new Error('A valid client IP address is required.'));
 
 		process.send({
 			type: 'connection',
@@ -60,9 +66,10 @@ Platform.prototype.notifyConnection = function (clientAddress, callback) {
 
 Platform.prototype.notifyDisconnection = function (clientAddress, callback) {
 	setImmediate(function () {
-		callback = callback || function () {};
+		callback = callback || function () {
+			};
 
-		if (!clientAddress) return callback(new Error('Client IP address is required.'));
+		if (!clientAddress || !isString(clientAddress)) return callback(new Error('A valid client IP address is required.'));
 
 		process.send({
 			type: 'disconnect',
@@ -75,7 +82,8 @@ Platform.prototype.notifyDisconnection = function (clientAddress, callback) {
 
 Platform.prototype.notifyClose = function (callback) {
 	setImmediate(function () {
-		callback = callback || function () {};
+		callback = callback || function () {
+			};
 
 		process.send({
 			type: 'close'
@@ -87,10 +95,11 @@ Platform.prototype.notifyClose = function (callback) {
 
 Platform.prototype.processData = function (clientAddress, data, callback) {
 	setImmediate(function () {
-		callback = callback || function () {};
+		callback = callback || function () {
+			};
 
-		if (!clientAddress) return callback(new Error('Client IP address is required.'));
-		if (!data) return callback(new Error('Data is required.'));
+		if (!clientAddress || !isString(clientAddress)) return callback(new Error('A valid client IP address is required.'));
+		if (!data || !isString(data)) return callback(new Error('A valid data is required.'));
 
 		process.send({
 			type: 'data',
@@ -104,11 +113,72 @@ Platform.prototype.processData = function (clientAddress, data, callback) {
 	});
 };
 
+Platform.prototype.sendMessageResponse = function (messageId, response, callback) {
+	setImmediate(function () {
+		callback = callback || function () {
+			};
+
+		if (!messageId || !isString(messageId)) return callback(new Error('A valid message id is required.'));
+		if (!response || !isString(response)) return callback(new Error('A valid response is required.'));
+
+		process.send({
+			type: 'response',
+			data: {
+				messageId: messageId,
+				response: response
+			}
+		});
+
+		callback();
+	});
+};
+
+Platform.prototype.sendMessageToDevice = function (device, message, callback) {
+	setImmediate(function () {
+		callback = callback || function () {
+			};
+
+		if (!device || !isString(device)) return callback(new Error('A valid device id is required.'));
+		if (!message || !isString(message)) return callback(new Error('A valid message is required.'));
+
+		process.send({
+			type: 'message',
+			data: {
+				device: device,
+				message: message
+			}
+		});
+
+		callback();
+	});
+};
+
+Platform.prototype.sendMessageToGroup = function (group, message, callback) {
+	setImmediate(function () {
+		callback = callback || function () {
+			};
+
+		if (!group || !isString(group)) return callback(new Error('A valid group id is required.'));
+		if (!message || !isString(message)) return callback(new Error('A valid message is required.'));
+
+		process.send({
+			type: 'message',
+			data: {
+				group: group,
+				message: message
+			}
+		});
+
+		callback();
+	});
+};
+
 Platform.prototype.log = function (title, description, callback) {
 	setImmediate(function () {
-		callback = callback || function () {};
+		callback = callback || function () {
+			};
 
-		if (!title) return callback(new Error('Log title is required.'));
+		if (!title || !isString(title)) return callback(new Error('A valid log title is required.'));
 
 		process.send({
 			type: 'log',
@@ -124,7 +194,8 @@ Platform.prototype.log = function (title, description, callback) {
 
 Platform.prototype.handleException = function (error, callback) {
 	setImmediate(function () {
-		callback = callback || function () {};
+		callback = callback || function () {
+			};
 
 		if (!error) return callback(new Error('Error is required.'));
 
