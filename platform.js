322deb036c3cee1f65909cc209a0df1c62278159
Benj Sicam <bsicam@reekoh.com>
2015-10-18 19:12:10 +0800

    Updated platform.js code

--- platform.js
+++ platform.js
@@ -3,10 +3,29 @@
 var inherits     = require('util').inherits,
 	EventEmitter = require('events').EventEmitter;
 
+/**
+ * Utility function to validate String Objects
+ * @param val The value to be evaluated.
+ * @returns {boolean}
+ */
 var isString = function (val) {
 	return typeof val === 'string' || ((!!val && typeof val === 'object') && Object.prototype.toString.call(val) === '[object String]');
 };
 
+/**
+ * Utility function to validate Error Objects
+ * @param val The value to be evaluated.
+ * @returns {boolean}
+ */
+var isError = function (val) {
+	return (!!val && typeof val === 'object') && typeof val.message === 'string' && Object.prototype.toString.call(val) === '[object Error]';
+};
+
+/**
+ * Main object used to communicate with the platform.
+ * @returns {Platform}
+ * @constructor
+ */
 function Platform() {
 	if (!(this instanceof Platform)) return new Platform();
 
@@ -23,6 +42,9 @@ function Platform() {
 
 inherits(Platform, EventEmitter);
 
+/**
+ * Init function for Platform.
+ */
 Platform.init = function () {
 	var self = this;
 
@@ -34,6 +56,10 @@ Platform.init = function () {
 	});
 };
 
+/**
+ * Needs to be called once in order to notify the platform that the plugin has already finished the init process.
+ * @param {function} [callback] Optional callback to be called once the ready signal has been sent.
+ */
 Platform.prototype.notifyReady = function (callback) {
 	callback = callback || function () {
 		};
@@ -41,44 +67,52 @@ Platform.prototype.notifyReady = function (callback) {
 	setImmediate(function () {
 		process.send({
 			type: 'ready'
-		});
-
-		callback();
+		}, callback);
 	});
 };
 
-Platform.prototype.notifyConnection = function (clientId, callback) {
+/**
+ * Notify the platform that the device has connected to this gateway. The platform will update the device status to online.
+ * @param {string} device The client or device identifier.
+ * @param {function} callback Optional callback to be called once the connection signal has been sent.
+ */
+Platform.prototype.notifyConnection = function (device, callback) {
 	callback = callback || function () {
 		};
 
 	setImmediate(function () {
-		if (!clientId || !isString(clientId)) return callback(new Error('A valid client/device identifier is required.'));
+		if (!device || !isString(device)) return callback(new Error('A valid client/device identifier is required.'));
 
 		process.send({
 			type: 'connection',
-			data: clientId
-		});
-
-		callback();
+			data: device
+		}, callback);
 	});
 };
 
-Platform.prototype.notifyDisconnection = function (clientId, callback) {
+/**
+ * Notify the platform that the device has disconnected from this gateway. The platform will update the device status to offline.
+ * @param {string} device The client or device identifier.
+ * @param {function} callback Optional callback to be called once the disconnect signal has been sent.
+ */
+Platform.prototype.notifyDisconnection = function (device, callback) {
 	callback = callback || function () {
 		};
 
 	setImmediate(function () {
-		if (!clientId || !isString(clientId)) return callback(new Error('A valid client/device identifier is required.'));
+		if (!device || !isString(device)) return callback(new Error('A valid client/device identifier is required.'));
 
 		process.send({
 			type: 'disconnect',
-			data: clientId
-		});
-
-		callback();
+			data: device
+		}, callback);
 	});
 };
 
+/**
+ * Notifies the platform that the server has closed. This will enable the platform to reboot this gateway.
+ * @param {function} callback Optional callback to be called once the close signal has been sent.
+ */
 Platform.prototype.notifyClose = function (callback) {
 	callback = callback || function () {
 		};
@@ -86,12 +120,16 @@ Platform.prototype.notifyClose = function (callback) {
 	setImmediate(function () {
 		process.send({
 			type: 'close'
-		});
-
-		callback();
+		}, callback);
 	});
 };
 
+/**
+ * Sends the device/sensor data to the platform to be processed.
+ * @param {string} device The client or device identifier.
+ * @param {string} data The JSON data to be processed.
+ * @param callback Optional callback to be called once the data has been sent.
+ */
 Platform.prototype.processData = function (device, data, callback) {
 	callback = callback || function () {
 		};
@@ -106,12 +144,17 @@ Platform.prototype.processData = function (device, data, callback) {
 				device: device,
 				data: data
 			}
-		});
-
-		callback();
+		}, callback);
 	});
 };
 
+/**
+ * Sends back a response to the message sent to the device through this gateway. These responses may be acknowledgement receipts that
+ * come from the devices connected to this gateway.
+ * @param {string} messageId The message id that was sent
+ * @param response
+ * @param callback
+ */
 Platform.prototype.sendMessageResponse = function (messageId, response, callback) {
 	callback = callback || function () {
 		};
@@ -126,12 +169,16 @@ Platform.prototype.sendMessageResponse = function (messageId, response, callback
 				messageId: messageId,
 				response: response
 			}
-		});
-
-		callback();
+		}, callback);
 	});
 };
 
+/**
+ * Send a message or command to a device.
+ * @param {string} device The device identifier to send the message or command to.
+ * @param {string} message The message or command to be sent to the device.
+ * @param {function} callback Optional callback to be called once the message has been sent.
+ */
 Platform.prototype.sendMessageToDevice = function (device, message, callback) {
 	callback = callback || function () {
 		};
@@ -146,18 +193,22 @@ Platform.prototype.sendMessageToDevice = function (device, message, callback) {
 				device: device,
 				message: message
 			}
-		});
-
-		callback();
+		}, callback);
 	});
 };
 
+/**
+ * Send a message or command to a group of devices.
+ * @param {string} group The device group name.
+ * @param {string} message The message or command to send to the group of devices.
+ * @param {function} callback Optional callback to be called once the message has been sent.
+ */
 Platform.prototype.sendMessageToGroup = function (group, message, callback) {
 	callback = callback || function () {
 		};
 
 	setImmediate(function () {
-		if (!group || !isString(group)) return callback(new Error('A valid group id is required.'));
+		if (!group || !isString(group)) return callback(new Error('A valid group name is required.'));
 		if (!message || !isString(message)) return callback(new Error('A valid message is required.'));
 
 		process.send({
@@ -166,37 +217,40 @@ Platform.prototype.sendMessageToGroup = function (group, message, callback) {
 				group: group,
 				message: message
 			}
-		});
-
-		callback();
+		}, callback);
 	});
 };
 
-Platform.prototype.log = function (title, description, callback) {
+/**
+ * Logs any data to the attached loggers in the topology.
+ * @param {string} data The data that needs to be logged.
+ * @param {function} callback Optional callback to be called once the data has been sent.
+ */
+Platform.prototype.log = function (data, callback) {
 	callback = callback || function () {
 		};
 
 	setImmediate(function () {
-		if (!title || !isString(title)) return callback(new Error('A valid log title is required.'));
+		if (!data || !isString(data)) return callback(new Error('A valid log data is required.'));
 
 		process.send({
 			type: 'log',
-			data: {
-				title: title,
-				description: description
-			}
-		});
-
-		callback();
+			data: data
+		}, callback);
 	});
 };
 
+/**
+ * Logs errors to all the attached exception handlers in the topology.
+ * @param {error} error The error to be handled/logged
+ * @param {function} callback Optional callback to be called once the error has been sent.
+ */
 Platform.prototype.handleException = function (error, callback) {
 	callback = callback || function () {
 		};
 
 	setImmediate(function () {
-		if (!error) return callback(new Error('Error is required.'));
+		if (!isError(error)) return callback(new Error('A valid error object is required.'));
 
 		process.send({
 			type: 'error',
@@ -205,7 +259,7 @@ Platform.prototype.handleException = function (error, callback) {
 				message: error.message,
 				stack: error.stack
 			}
-		});
+		}, callback);
 	});
 };
 
